be46d8a134d65e8e87aea375b1cd296b14a156f8
heck <chris@triadms.com>
2016-04-15 15:48:55 -0400

    first hello world commit

---
+++ bot.js
@@ -0,0 +1,29 @@
+var Botkit = require('botkit')
+
+// Expect a SLACK_TOKEN environment variable
+var slackToken = process.env.SLACK_TOKEN
+if (!slackToken) {
+  console.error('SLACK_TOKEN is required!')
+  process.exit(1)
+}
+
+var controller = Botkit.slackbot()
+var bot = controller.spawn({
+  token: slackToken
+})
+
+bot.startRTM(function (err, bot, payload) {
+  if (err) {
+    throw new Error('Could not connect to Slack')
+  }
+})
+
+console.log('hi');
+
+controller.on('bot_channel_join', function (bot, message) {
+  bot.reply(message, "I'm here!")
+})
+
+controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
+  bot.reply(message, 'Hello.')
+})
